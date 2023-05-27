1891b1e6f7c2feb905d9e67b8e64725cd735476c
batmunkh0612 <cypox2017@gmail.com>
2023-05-28 01:33:53 +0800

    tedt

--- backend/index.js
+++ backend/index.js
@@ -2,38 +2,40 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import router from "./router/post.js";
-import routerUser from "./router/user.js";
-import routerComment from "./router/comment.js";
-import routerAd from "./router/ad.js";
+import postRouter from "./router/post.js";
+import userRouter from "./router/user.js";
+import commentRouter from "./router/comment.js";
+import adRouter from "./router/ad.js";
 
 dotenv.config();
+
 const uri = process.env.MONGO_URI || "";
 const port = process.env.PORT || 6969;
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/Post", router);
-app.use("/User", routerUser);
-app.use("/Comment", routerComment);
-app.use("/Ad", routerAd);
 
-const connect = () => {
-  mongoose.set("strictQuery", false);
-  mongoose
-    .connect(uri, {})
+// Routes
+app.use("/post", postRouter);
+app.use("/user", userRouter);
+app.use("/comment", commentRouter);
+app.use("/ad", adRouter);
 
-    .then(() => {
-      console.log("connected to DB");
-    })
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+// Database Connection
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(uri, {});
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+  }
 };
 
+// Start the server
 app.listen(port, async () => {
-  console.log(`app running ${port}`);
-  connect();
+  await connectToDatabase();
+  console.log(`Server is running on port ${ port }`);
 });
